90fe4e7bb3fa69ce188c66fc5a8448495cf9df8c
Nightowl5275 <139814989+Nightowl5275@users.noreply.github.com>
2026-03-08 18:03:51 -0400

    not scoring during fuel collection

--- src/main/java/frc/robot/commands/autos/AutoCenterCollectWOInterferance.java
+++ src/main/java/frc/robot/commands/autos/AutoCenterCollectWOInterferance.java
@@ -10,6 +10,7 @@ import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.intake.IntakeRotate;
 import frc.robot.commands.intake.IntakeRunAndRotate;
 import frc.robot.commands.intake.RunIntakeWheels;
@@ -26,7 +27,7 @@ import frc.robot.util.NetworkTables.IntakeTable;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoCenterCollectWOInterferance extends ParallelCommandGroup {
+public class AutoCenterCollectWOInterferance extends SequentialCommandGroup {
   /** Creates a new AutoCenterCollectAndShootFullPath. */
   public AutoCenterCollectWOInterferance(Shooter shooter, Hood hood, Indexer indexer, SwerveSubsystem swerve, Vision vision, Hopper hopper, Intake intake) {
     // Add your commands in the addCommands() call, e.g.
@@ -37,6 +38,8 @@ public class AutoCenterCollectWOInterferance extends ParallelCommandGroup {
     } catch(Exception e){
         path = Commands.none();
     }
-    addCommands(new IntakeRotate(intake, false), path.withTimeout(1.5), new RunIntakeWheels(intake, IntakeTable.kAutoInPower), new Score(shooter, hood, indexer, swerve, vision, hopper, intake));
+    addCommands(Commands.parallel(new IntakeRotate(intake, false),
+    path.withTimeout(1.5), new RunIntakeWheels(intake, IntakeTable.kAutoInPower)),
+    new Score(shooter, hood, indexer, swerve, vision, hopper, intake));
   }
 }
