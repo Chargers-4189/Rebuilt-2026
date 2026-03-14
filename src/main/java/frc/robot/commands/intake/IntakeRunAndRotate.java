c035a039f214284ed327d76679083541d2d3159a
Drive Computer <122412361+Chargers4189@users.noreply.github.com>
2026-03-14 18:27:41 -0400

    Intake Fix

--- src/main/java/frc/robot/commands/intake/IntakeRunAndRotate.java
+++ src/main/java/frc/robot/commands/intake/IntakeRunAndRotate.java
@@ -19,7 +19,7 @@ public class IntakeRunAndRotate extends SequentialCommandGroup {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new IntakeRotate(intake, false),
+      new IntakeRotate(intake, true),
       new RunIntakeWheels(intake, power)
     );
   }
