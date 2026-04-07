2362ed00c79233a3e1ac0e2749bb4044d64f54b2
Nightowl5275 <139814989+Nightowl5275@users.noreply.github.com>
2026-04-07 00:47:47 -0400

    did light stuff

--- src/main/java/frc/robot/subsystems/IntakeWheels.java
+++ src/main/java/frc/robot/subsystems/IntakeWheels.java
@@ -8,6 +8,10 @@ import java.util.function.DoubleSupplier;
 
 import com.ctre.phoenix6.configs.TalonFXSConfiguration;
 import com.ctre.phoenix6.hardware.TalonFXS;
+import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+
 import com.ctre.phoenix6.signals.MotorArrangementValue;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -19,6 +23,10 @@ import frc.robot.util.NetworkTables.IntakeTable;
 public class IntakeWheels extends SubsystemBase {
   /** Creates a new Intake. */
   private TalonFXS wheelMotor = new TalonFXS(Constants.IntakeConstants.kWheelMotor); //Needs to be inverted
+  private VictorSP LEDone = new VictorSP(Constants.IntakeConstants.kLEDONECANID);
+  private PWMTalonSRX LEDtwo = new PWMTalonSRX(Constants.IntakeConstants.kLEDTWOCANID);
+  private int timer = 0;
+
 
 
   public IntakeWheels() {
@@ -32,6 +40,10 @@ public class IntakeWheels extends SubsystemBase {
     wheelMotor.set(-power);
   }
 
+  public double getWheelPower() {
+    return wheelMotor.get();
+  }
+
   public Command runWheelsCommand(DoubleSupplier power) {
     return Commands.run(
       () -> setWheelPower(power.getAsDouble()), this
@@ -42,8 +54,26 @@ public class IntakeWheels extends SubsystemBase {
     );
   }
 
+
+  public void setDriverLight() {
+    if(getWheelPower() > 0.1){
+      if ((timer % 25) < 12.5) { // 1/2 seconds flash
+        LEDone.set(1);
+        LEDtwo.set(1);
+      } else {
+        LEDone.set(0);
+        LEDtwo.set(0);
+      }
+    }else{
+      LEDone.set(0);
+      LEDtwo.set(0);    
+    }
+  }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    setDriverLight();
+    timer++;
   }
 }
